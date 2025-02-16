20821d6fc98fbda986519a652b0ef518419cdb77
laupommares <laurapommares@gmail.com>
2025-02-16 15:52:50 -0300

    Ajustes card banner home.

--- resources/views/livewire/pages/home.blade.php
+++ resources/views/livewire/pages/home.blade.php
@@ -2,16 +2,16 @@
 @extends('livewire.layouts.app')
 @section('content')
     <header id="header" class="bg-cover bg-center h-[880px]" style="background-image: url('/header.png');">
-        <div class="mx-auto px-4 sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] 2xl:max-w-[1320px] flex justify-end h-full items-end">
-            <div class="mb-10 card card-compact max-w-[462px] h-[600px] shadow-xl bg-white">
+        <div class="relative mx-auto px-4 sm:max-w-[540px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1140px] 2xl:max-w-[1320px] flex justify-end h-full items-end">
+            <div class="mb-10 card card-compact max-w-[462px] shadow-xl bg-white absolute bottom-0">
                 <figure class="relative w-full p-4  ">
-                    <img class="inset-0 w-full object-cover h-[378px] rounded-ms" src="{{ asset('tunos.jpg') }}" alt="">
+                    <img class="inset-0 w-full object-cover object-top h-[340px]" src="{{ asset('tunos.jpg') }}" alt="">
                 </figure>
                 <div class="card-body text-dark bg-white">
                     <h2 class="card-title">¡Comenzá hoy!</h2>
-                    <p class="text-base ">Diseño planes pensados para acompañarte a lograr el bienestar que mereces, respetando tus necesidades únicas. ¡Este es el primer paso hacia tu mejor versión!</p>
+                    <p class="text-base">Diseño planes pensados para acompañarte a lograr el bienestar que mereces, respetando tus necesidades únicas. ¡Este es el primer paso hacia tu mejor versión!</p>
                     <div class="card-actions justify-end">
-                        <button class="text-dark w-[430px] h-12 font-semibold bg-green px-4 py-2 rounded-sm">Saca turno acá!</button>
+                        <button class="text-dark w-[430px] h-12 font-semibold bg-green px-4 py-2 rounded-sm text-xl ">Saca turno acá!</button>
                     </div>
                 </div>
             </div>    

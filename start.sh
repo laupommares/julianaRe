3e65cd8031810fede074cdf84c2312ed45379980
laupommares <laurapommares@gmail.com>
2025-06-28 15:45:02 -0300

    cambios start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
+# Forzar uso del .env
 cp .env.example .env
 
+# Limpiar caches por si quedó algo viejo
+php artisan config:clear
+php artisan cache:clear
+php artisan route:clear
+php artisan view:clear
+
+# Instalar dependencias
 composer install --no-dev --optimize-autoloader
 
-php artisan config:clear
+# Claves y migraciones
 php artisan key:generate
 php artisan migrate --force
+
+# Cachear configuración de nuevo
 php artisan config:cache
 php artisan route:cache
 
+# Iniciar el servidor en el puerto que Render espera
 php artisan serve --host=0.0.0.0 --port=$PORT
